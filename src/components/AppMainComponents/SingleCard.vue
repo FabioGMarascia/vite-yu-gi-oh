0ccc4b60991b834c6c973e5f98d052307856f6d7
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-27 17:09:26 +0200

    M-1 and style

--- src/components/AppMainComponents/SingleCard.vue
+++ src/components/AppMainComponents/SingleCard.vue
@@ -1,10 +1,31 @@
 <script>
 export default {
 	name: "SingleCard",
-	components: {},
+	props: ["element"],
 };
 </script>
 
-<template></template>
+<template>
+	<div class="col-2 mx-2 mb-3 px-0">
+		<img :src="element.card_images[0].image_url" />
 
-<style scoped></style>
+		<div class="cardInfoBox text-center py-4">
+			<h2 class="text-white fw-bold">{{ element.name }}</h2>
+			<span>{{ element.race }}</span>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+img {
+	width: 100%;
+}
+
+.cardInfoBox {
+	background-color: #d48f38;
+}
+
+h2 {
+	font-size: 1rem;
+}
+</style>
