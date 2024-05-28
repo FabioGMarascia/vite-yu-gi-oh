d9c460eca2e8779d296981448027d92fba90489b
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-28 22:54:11 +0200

    changed click event whit input event

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,27 @@ export default {
 	},
 	data() {
 		return {
-			cardUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=200&offset=0",
+			cardUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=300&offset=0",
 			archetypeUrl: "https://db.ygoprodeck.com/api/v7/archetypes.php",
 			selected: "",
 			store,
 			allCards: [],
 		};
+	},
+	methods: {
+		getAcrchetype() {
+			setTimeout(() => {
+				if (this.selected != "") {
+					axios
+						.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=" + this.selected)
+						.then((result) => {
+							this.store.cards = result.data.data;
+						});
+				} else {
+					this.store.cards = this.allCards;
+				}
+			}, 1);
+		},
 	},
 	created() {
 		axios.get(this.cardUrl).then((result) => {
@@ -30,38 +45,19 @@ export default {
 			this.store.archetypeList = result.data;
 		});
 	},
-	methods: {
-		getAcrchetype() {
-			if (this.selected != "") {
-				axios
-					.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=" + this.selected)
-					.then((result) => {
-						this.store.cards = result.data.data;
-					});
-			} else {
-				this.store.cards = this.allCards;
-			}
-		},
-	},
 };
 </script>
 
 <template>
 	<AppHeader />
 	<div class="mainBox py-4">
-		<div class="row justify-content-center gap-2">
-			<div class="col-2 px-0 mb-4">
-				<select class="form-select fw-bold w-100" v-model="selected">
-					<option value="">Select the archetype</option>
-					<option v-for="archetype in store.archetypeList">
-						{{ archetype.archetype_name }}
-					</option>
-				</select>
-			</div>
-
-			<div class="col-1 px-0">
-				<button class="btn w-100 fw-bold bg-white" @click="getAcrchetype">SEARCH</button>
-			</div>
+		<div class="col-2 px-0 mb-4 mx-auto">
+			<select class="form-select fw-bold w-100" v-model="selected" @input="getAcrchetype">
+				<option value="">Select the archetype</option>
+				<option v-for="archetype in store.archetypeList">
+					{{ archetype.archetype_name }}
+				</option>
+			</select>
 		</div>
 
 		<AppMain />
